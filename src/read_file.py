006b150699773f45dacb872a035636608ce2cf1b
yurilopes <yuh.lopes@gmail.com>
2017-05-15 21:36:23 -0300

    Ajustada leitura do arquivo

--- src/read_file.py
+++ src/read_file.py
@@ -4,6 +4,7 @@ with gzip.open('../datasets/train/1_threshold.txt.gz', 'rb') as f:
     file_content = f.readlines()
     # print file_content
     linha_1 = file_content[0]
-    lista_linha_1 = linha_1.split(',')
+    lista_linha_1 = linha_1.replace(" ", "").replace("\n", "").split(',')
+    lista_linha_1 = map(int, lista_linha_1)
     print lista_linha_1
-    print len(linha_1)
+    print len(lista_linha_1)

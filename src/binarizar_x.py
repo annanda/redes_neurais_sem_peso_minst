c6f321de0706529e86a58b2c1a1d697a03c1d127
Annanda Sousa <annanda.sousa@gmail.com>
2017-05-18 21:10:55 -0300

    Para binarizar o x com apenas um valor de threshold

---
+++ src/binarizar_x.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import fetch_mldata
+import csv
+
+mnist = fetch_mldata('MNIST original')
+
+X = mnist.data
+
+
+def binarizar_x(xd, threshold):
+    result = list(xd)
+    for i in xrange(len(result)):
+        for j in xrange(len(result[i])):
+            if result[i][j] >= threshold:
+                result[i][j] = 1
+            else:
+                result[i][j] = 0
+    return result
+
+
+def create_file(file_name, content):
+    with open('../binary_x/' + file_name + '.csv', 'wb') as csv_file:
+        spamwriter = csv.writer(csv_file, delimiter=',')
+        for line in content:
+            spamwriter.writerow(line)
+
+
+def main():
+    treashold = 1
+    result = binarizar_x(X, treashold)
+    create_file(str(treashold), result)
+
+if __name__ == '__main__':
+    main()
+
+
